c82b1a6d66bc853b28daa041096c2213e5f8bce2
FLambda <hg@flambda.de>
2015-04-14 21:12:27 +0200

    Fixed Error:(12, 24) Gradle: error: cannot reference this before supertype constructor has been called

--- Tabletapp/app/src/main/java/database/low_level_adapter.java
+++ Tabletapp/app/src/main/java/database/low_level_adapter.java
@@ -6,20 +6,16 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class low_level_adapter extends SQLiteOpenHelper {
 
-    /*
+
     public low_level_adapter(Context context) {
-        //TODO: error: connot reference this before suptertype constructir has been called
-        super(context, this.db_layout.NAME, null, this.db_layout.VERSION);
+        super(context, layout.NAME, null, layout.VERSION);
 
-    }*/
-
-   // TODO: added this to fix the issue
-    public static final layout db_layout = new layout();
+    }
 
     public low_level_adapter(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
-   //
+
 
     //always call interface ready before doing anything else!
     //once patching is in place, initalizing the db might take some time
@@ -30,7 +26,7 @@ public class low_level_adapter extends SQLiteOpenHelper {
 
 
     public void onCreate(SQLiteDatabase db) {
-        for (table tmp : this.db_layout.tables) {
+        for (table tmp : layout.tables) {
             db.execSQL(tmp.getSqliteDescription());
         }
     }
@@ -38,14 +34,14 @@ public class low_level_adapter extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //no patching yet, we jsut drop the table
         //for release, we will need a patching system in place
-        for (table tmp : this.db_layout.tables) {
+        for (table tmp : layout.tables) {
             db.execSQL("DROP TABLE " + tmp.getName() + ";" );
         }
         this.onCreate(db);
     }
 
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        for (table tmp : this.db_layout.tables) {
+        for (table tmp : layout.tables) {
             db.execSQL("DROP TABLE " + tmp.getName() + ";" );
         }
         this.onCreate(db);
--- Tabletapp/app/src/main/java/database/object_level_db.java
+++ Tabletapp/app/src/main/java/database/object_level_db.java
@@ -1,6 +1,7 @@
 package database;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -26,10 +27,11 @@ public class object_level_db {
             //trow exception so we know we haven't opened the interface yet!
         }
     }
-/* TODO: outcommentet because of bug
+
     public object_level_db(Context ctx) {
         this.db_helper = new low_level_adapter(ctx);
-    }*/
+    }
+
 
     public void open() {
        this.opened = true;
